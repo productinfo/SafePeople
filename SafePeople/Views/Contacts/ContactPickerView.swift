7ef3028517c503263b729125adc0efe0633504a5
brittanyarima <brittanyarima@gmail.com>
2022-12-01 21:33:55 -0800

    Refactor

--- SafePeople/Views/Contacts/ContactPickerView.swift
+++ SafePeople/Views/Contacts/ContactPickerView.swift
@@ -15,18 +15,9 @@ struct ContactPickerView: View {
     @State var contacts = [Contact.init(firstName: "", lastName: "", phoneNumber: [""])]
     @State var showContactNumberSheet = false
     @State var selectedNumber: String = ""
-    
-    // Search Functionality
     @State private var searchText = ""
-    
 
-
-
-    
-    
     var body: some View {
-        
-        
         NavigationView {
             List {
                 ForEach (self.contacts.filter({ (cont) -> Bool in
@@ -34,63 +25,52 @@ struct ContactPickerView: View {
                     "\(cont)".lowercased().contains(self.searchText.lowercased())
                 })) { contact in
                     ContactRow(contact: contact, showPicker: $showPicker, selectedContact: $selectedContact)
-                    
                 }
-              
             }
             .onAppear() {
                 self.requestAccess()
             }
-        
-        .listStyle(GroupedListStyle())
-        .navigationTitle("All Contacts".localized)
-        .navigationBarItems(trailing: Button(action: {self.showPicker = false}, label: {
-            Text("Cancel".localized)
-        }))
-        .searchable(text: $searchText, placement: .navigationBarDrawer(displayMode: .always))
-        .sheet(isPresented: $showContactNumberSheet) {
-            
-            ContactDetailSheet(selectedContact: selectedContact, showPickerView: $showPicker)
-        }
-       
+            .listStyle(GroupedListStyle())
+            .navigationTitle("All Contacts".localized)
+            .navigationBarItems(trailing: Button(action: {self.showPicker = false}, label: {
+                Text("Cancel".localized)
+            }))
+            .searchable(text: $searchText, placement: .navigationBarDrawer(displayMode: .always))
+            .sheet(isPresented: $showContactNumberSheet) {
+                ContactDetailSheet(selectedContact: selectedContact, showPickerView: $showPicker)
+            }
         }
         .onChange(of: selectedContact) { selected in
-           // Show sheet of contact info & ability to select number
+            // Show sheet of contact info & ability to select number
             showContactNumberSheet.toggle()
-
         }
-        
-        
+    }
+
+    func getContacts() {
+        DispatchQueue.main.async {
+            self.contacts = contactsProvider.fetchContacts()
+        }
     }
     
-    
-    //MARK: - FUNCTIONS
-    func getContacts() {
-            DispatchQueue.main.async {
-                self.contacts = contactsProvider.fetchContacts()
-            }
-        }
-    
     func requestAccess() {
-            let store = CNContactStore()
-            switch CNContactStore.authorizationStatus(for: .contacts) {
-            case .authorized:
-                self.getContacts()
-            case .denied:
-                store.requestAccess(for: .contacts) { granted, error in
-                    if granted {
-                        self.getContacts()
-                    }
+        let store = CNContactStore()
+        switch CNContactStore.authorizationStatus(for: .contacts) {
+        case .authorized:
+            self.getContacts()
+        case .denied:
+            store.requestAccess(for: .contacts) { granted, error in
+                if granted {
+                    self.getContacts()
                 }
-            case .restricted, .notDetermined:
-                store.requestAccess(for: .contacts) { granted, error in
-                    if granted {
-                        self.getContacts()
-                    }
-                }
-            @unknown default:
-                print("error")
             }
+        case .restricted, .notDetermined:
+            store.requestAccess(for: .contacts) { granted, error in
+                if granted {
+                    self.getContacts()
+                }
+            }
+        @unknown default:
+            print("error")
         }
-    
+    }
 }
