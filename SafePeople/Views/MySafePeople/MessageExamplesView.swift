dcf68518f269813acbcbf8aea06dee4eb1baa506
brittanyarima <brittanyarima@gmail.com>
2022-12-01 21:47:40 -0800

    Cleanup

--- SafePeople/Views/MySafePeople/MessageExamplesView.swift
+++ SafePeople/Views/MySafePeople/MessageExamplesView.swift
@@ -12,29 +12,18 @@ struct MessageExamplesView: View {
         ScrollView(.vertical, showsIndicators: false) {
             VStack {
                 MessageExampleCardView(title: "General", text: "Hi! I've hit my 'SAFE' button on the Safe People app because I'm really struggling with ___ and need to know I'm not alone.")
-                
                 MessageExampleCardView(title: "General", text: "Hi! I've hit my 'SAFE' button on the Safe People app because I feel like giving up right now.")
-                
                 MessageExampleCardView(title: "General", text: "Hi! I've hit my 'SAFE' button on the Safe People app because I feel like I'm falling apart and am reaching out for support.")
-                
                 MessageExampleCardView(title: "General", text: "Hi! I've hit my 'SAFE' button on the Safe People app because I'm in a situation where I know I could easily be tempted or triggered.")
-                
                 MessageExampleCardView(title: "Addiction", text: "Hi! I've hit my 'SAFE' button on the Safe People app because I'm feeling really tempted right now and need some support.")
-                
                 MessageExampleCardView(title: "Anxiety", text: "Hi! I've hit my 'SAFE' button on the Safe People app because I'm dealing with anxiety right now and need someone to talk to.")
-                
                 MessageExampleCardView(title: "Food/Diet", text: "Hi! I've hit my 'SAFE' button on the Safe People app because I'm being tempted with food and need some accountability.")
-                
                 MessageExampleCardView(title: "Triggered", text: "Hi! I've hit my 'SAFE' button on the Safe People app because I've been triggered and in need of support.")
-                
-                
-                
             }
             .padding()
         }
         .navigationTitle("Message Examples")
     }
-        
 }
 
 struct MessageExamplesView_Previews: PreviewProvider {
